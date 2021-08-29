7d4a94bde6c996ee88a2726e9459752ff8c001b1
Nguyen Tien Anh <nguyentienanh2907@gmail.com>
2021-08-30 01:42:36 +0700

    29/8 update

--- js/validate-register.js
+++ js/validate-register.js
@@ -1,60 +1,65 @@
+const fullname = document.forms["register-form"]['fullname'].value
+const username = document.forms["register-form"]['username'].value
+const password = document.forms["register-form"]['password'].value
+const confirmPassword = document.forms["register-form"]['confirm-password'].value
+
 function isEmpty(str) {
     return (str == '' || str == null || str == undefined) ? true : false
 }
 
-function errorDisplay(idElement, error){ 
+function errorDisplayById(idElement, error) {
     document.getElementById(idElement).innerHTML = error
 }
 
-function validate() {
-    const fullname = document.forms["register-form"]['fullname'].value
-    const username = document.forms["register-form"]['username'].value
-    const password = document.forms["register-form"]['password'].value
-    const confirmPassword = document.forms["register-form"]['confirm-password'].value
-    let error = []
-    const RegEx = [
-        'fullname' = "[A-Za-z]+([\ A-Za-z]+)*$/gm",
-        'username' = "/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/igm",
-        'password' = "/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm"
-    ]
+function validator() {
+    var error = []
+    const RegEx = {
+        fullname: "[A-Za-z]+([\ A-Za-z]+)*$/gm",
+        username: "/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/igm",
+        password: "/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm"
+    }
     // validate text
     if (isEmpty(fullname)) {
-        error['fullname'] = "Please enter your full name"
-        document.getElementById("error__fullname").innerHTML = error['fullname']
-    } else if (!RegEx["fullname"].test(fullname)) {
-        error['fullname'] = "Name invalid"
-        document.getElementById("error__fullname").innerHTML = error['fullname']
+        error.push("Please enter your full name")
+        errorDisplayById("error__fullname", error)
+    } else if (!RegEx.fullname.test(fullname)) {
+        error.push("Name invalid")
+        errorDisplayById("error__fullname", error)
     }
 
     //validate username
     if (isEmpty(username)) {
-        error['username'] = "Please enter the username"
-        document.getElementById("error__username").innerHTML = error['username']
-    } else if (!RegEx['username'].test(username)) {
-        error['username'] = "Username invalid"
-        document.getElementById("error__username").innerHTML = error['username']
+        error.push("Please enter the username")
+        errorDisplayById("error__username", error)
+    } else if (!RegEx.username.test(username)) {
+        error.push("Username invalid")
+        errorDisplayById("error__username", error)
     }
 
     // validate password
     if (isEmpty(password)) {
-        error['password'] = "Please enter password"
-    } else if (!RegEx['password'].test(password)) {
-        error['password'] = "Password invalid"
+        error.push("Please enter password")
+        errorDisplayById("error__password", error)
+
+    } else if (!RegEx.password.test(password)) {
+        error.push("Password invalid")
+        errorDisplayById("error__password", error)
+
     }
 
     // validate confirm password
     if (isEmpty(confirmPassword)) {
-        error['confirm password'] = "Please enter confirm password"
+        error.push("Please enter confirm password")
+        errorDisplayById("error__confirmPassword", error)
     } else if (confirmPassword != password) {
-        error['confirm password'] = "Password do not match"
+        error.push("Password do not match")
+        errorDisplayById("error__confirmPassword", error)
     }
 
     //result validate
-    if (error.length > 0) {
-
-        document.getElementById("error__password").innerHTML = error['password']
-        document.getElementById("error__confirmPassword") = error['confirmPassword']
-        error.splice(0, error.length);
-        document.getElementsByTagName("form").onsubmit = function () { return false }
-    }
+    if(error.length > 0){
+        alert("co loi")
+        return false
+    } 
+    return false
 }
--- register.php
+++ register.php
@@ -6,54 +6,57 @@ define('FULLNAME', '/^[A-Za-z]+([\ A-Za-z]+)*$/m');
 define('USENAME', '/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/im');
 define('PASSWORD', '/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/m');
 
-function validate($post)
-{
-   $error = [];
-    $post = (object)$post;
-    if (empty($post->fullname)) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username =  $_POST['username'];
+    $fullname = $_POST['fullname'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirmPassword'];
+
+    $error = [];
+    if (empty($fullname)) {
         $error['fullname'] = "Please enter your full name";
-    } else if (!preg_match(FULLNAME, $post->fullname)) {
+    } else if (!preg_match(FULLNAME, $fullname)) {
         $error['fullname'] = "Name invalid";
     }
 
-    if (empty($post->username)) {
-        $error['usernae'] = 'Please enter the username';
-    } else if (!preg_match(USENAME, $post->username)) {
+    if (empty($username)) {
+        $error['username'] = "Please enter the username";
+    } else if (!preg_match(USENAME, $username)) {
         $error['username'] = "Username invalid";
     }
-    if (empty($post->password)) {
+
+    $selectUsername = "SELECT `username` FROM `account`";
+    foreach ($conn->query($selectUsername) as $data) {
+        if ($username == $data) {
+            $error['username'] = "Username already exists ";
+            break;
+        }
+    }
+
+    if (empty($password)) {
         $error['password'] = "Please enter password";
-    } else if (!preg_match(PASSWORD, $post->password)) {
+    } else if (!preg_match(PASSWORD, $password)) {
         $error['password'] = "Password invalid";
     }
 
     // validate confirm password
-    if (empty($post->confirmPassword)) {
+    if (empty($confirmPassword)) {
         $error['confirm password'] = "Please enter confirm password";
-    } else if ($post->confirmPassword != $post->password) {
+    } else if ($confirmPassword != $password) {
         $error['confirm password'] = "Password do not match";
     }
-   
-}
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $user =  $_POST['username'];
-    $fullname = $_POST['fullname'];
-    $pass = $_POST['password'];
-    $confirmpassword = $_POST['confirmPassword'];
- 
-    validate($_POST);
     if (empty($error)) {
         $pass = md5($pass);
         $sql = "INSERT INTO `account`(`username`, `fullname`, `password`) 
-          VALUES('$user','$fullname','$pass');";
+          VALUES('$user','$fullname','$pass')";
         if (mysqli_query($conn, $sql)) {
             echo "Register successfully";
         } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
         }
     } else {
-        echo 'Xac thuc pass ko dung';
+        echo 'Loi validate';
     }
 }
 ?>
@@ -68,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/register.css">
-   
+ 
     <script src="https://kit.fontawesome.com/10b69a6de3.js" crossorigin="anonymous"></script>
 </head>
 
@@ -76,31 +79,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <div class="wrapper">
         <div class="title">Register</div>
-        <form method="POST" action="/register" name="register-form" onsubmit="return validate()">
+        <form method="POST" action="/register" name="register-form" onSubmit="return validator()">
             <div class="text-field">
                 <label>Fullname</label>
                 <input type="text" name="fullname" id="name">
-                <p id="error__fullname" class="validate-error"></p>
+                <span id="error__fullname" class="validate-error"></span>
             </div>
             <div class="text-field">
                 <label>Username</label>
                 <input type="text" name="username" id="usename">
-                <p id="error__username" class="validate-error"></p>
+                <span id="error__username" class="validate-error"></span>
             </div>
             <div class="text-field">
                 <label>Password</label>
                 <input type="password" name="password" id="password">
-                <p id="error__password" class="validate-error"></p>
+                <span id="error__password" class="validate-error"></span>
             </div>
             <div class="text-field">
                 <label>Confirm Password</label>
                 <input type="password" name="confirmPassword" id="confirmPassword">
-                <p id="error__confirmPassword" class="validate-error"></p>
+                <span id="error__confirmPassword" class="validate-error"></span>
             </div>
             <button onclick="validate()">Register</button>
         </form>
     </div>
- <script type="text/javascript" src="./js/validate-register.js"></script>
+    <script type="text/javascript" src="./js/validate-register.js"></script>
 </body>
 
 </html>
